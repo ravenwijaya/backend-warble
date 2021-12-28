a02da526f562d8edb04b3928484a4c0b7ae8130a
Ong William Raven Wijaya <58552967+ravenwijaya@users.noreply.github.com>
2021-12-28 15:39:23 +0700

    first commit

---
+++ handlers/auth.js
@@ -0,0 +1,72 @@
+const db = require("../models");
+const jwt = require("jsonwebtoken");
+
+exports.signin = async function (req, res, next) {
+  try {
+    console.log("asd")
+    let user = await db.User.findOne({
+      email: req.body.email,
+    });
+    console.log(user);
+    let { id, username, profileImageUrl } = user;
+    let isMatch = await user.comparePassword(req.body.password);
+    console.log(isMatch);
+    if (isMatch) {
+      let token = jwt.sign(
+        {
+          id,
+          username,
+          profileImageUrl,
+        },
+        process.env.SECRET_KEY
+      );
+      return res.status(200).json({
+        id,
+        username,
+        profileImageUrl,
+        token,
+      });
+    } else {
+      return next({
+        status: 400,
+        message: "Invalid Email/Password.",
+      });
+    }
+  } catch (err) {
+    return next({
+      status: 400,
+      message: "Invalid Email/Password.",
+    });
+  }
+};
+
+exports.signup = async function (req, res, next) {
+  try {
+    let user = await db.User.create(req.body);
+    console.log(user);
+    let { id, username, profileImageUrl } = user;
+   
+    let token = jwt.sign(
+      {
+        id,
+        username,
+        profileImageUrl,
+      },
+      process.env.SECRET_KEY
+    );
+    return res.status(200).json({
+      id,
+      username,
+      profileImageUrl,
+      token,
+    });
+  } catch (err) {
+    if (err.code === 11000) {
+      err.message = "Sorry, that username and/or email is taken";
+    }
+    return next({
+      status: 400,
+      message: err.message,
+    });
+  }
+};
